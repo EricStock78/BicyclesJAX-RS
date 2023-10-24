cae6ef532addef32db1968a3ff3f5e00bfde2e9f
ericdstock <ericdstock@gmail.com>
2023-10-24 15:01:10 -0230

    Setup API route to return list of data converted to JSON

---
+++ src/main/java/com/example/restproject/Bicycle.java
@@ -0,0 +1,15 @@
+package com.example.restproject;
+
+public class Bicycle {
+    public String name;
+    public int speeds;
+
+    Bicycle(String name, int speeds) {
+        this.name = name;
+        this.speeds = speeds;
+    }
+
+    Bicycle() {
+
+    }
+}
--- src/main/java/com/example/restproject/HelloResource.java
+++ src/main/java/com/example/restproject/HelloResource.java
@@ -3,12 +3,25 @@ package com.example.restproject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.GenericEntity;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello-world")
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/bicycles")
 public class HelloResource {
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response bicycles() {
+        Bicycle blueBike = new Bicycle("Blue Bike", 10);
+        Bicycle redBike = new Bicycle("Red Bike", 12);
+        List<Bicycle> bikeList = new ArrayList<Bicycle>();
+
+        GenericEntity<List<Bicycle>> entityList = new GenericEntity<List<Bicycle>>(bikeList) {};
+        bikeList.add(blueBike);
+        bikeList.add(redBike);
+        return Response.ok(entityList).build();
     }
 }
